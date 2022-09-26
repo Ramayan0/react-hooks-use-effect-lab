b48c0142e1ef5a7a0cabdaea4c5dc8ae69290d8a
hamsa <hafieekhan@gmail.com>
2022-09-26 15:28:11 +0300

    "Completed Lab"

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,9 +1,22 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 function Question({ question, onAnswered }) {
   const [timeRemaining, setTimeRemaining] = useState(10);
 
   // add useEffect code
+  useEffect(() => {
+    if (timeRemaining === 0) {
+      setTimeRemaining(10);
+      onAnswered(false);
+      return;
+    }
+    const timer = setInterval(() => {
+      setTimeout(() => {
+        setTimeRemaining((timeRemaining) => timeRemaining - 1);
+      }, 1000);
+    });
+    return () => clearInterval(timer);
+  }, [timeRemaining, onAnswered]);
 
   function handleAnswer(isCorrect) {
     setTimeRemaining(10);
